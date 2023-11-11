9c221b40bb91d8707410b25ad6b0cd10f9516b7c
Abstract Programmer <AbstractProggrammer@example.com>
2023-11-11 15:03:32 +0800

    js added

---
+++ src/app.js
@@ -0,0 +1,16 @@
+// input Element
+const cardName = document.getElementById('card-name').value;
+const cardNumber = document.getElementById('card-number').value;
+const cardMonth = document.getElementById('month').value;
+const cardYear = document.getElementById('year').value;
+const cvcNumber = document.getElementById('cvc-number').value
+
+
+//Container for state
+const formInput = document.getElementById('form');
+const greetForm = document.getElementById('thankyou');
+
+
+//button
+const btnSubmit = document.getElementById('submit')
+const btnThnks = document.getElementById('submitThankyou')
